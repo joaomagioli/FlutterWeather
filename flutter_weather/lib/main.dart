3883012d00ba2149b53e4384bbd84c174852e86c
Joao Guilherme <joaomagioli@gmail.com>
2019-05-25 22:38:01 -0300

    bloc pattern in progress

--- flutter_weather/lib/main.dart
+++ flutter_weather/lib/main.dart
@@ -1,19 +1,36 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_weather/repository/weather_api_client.dart';
+import 'package:flutter_weather/repository/weather_repository.dart';
+import 'simple_bloc_delegate.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() {
+  BlocSupervisor().delegate = SimpleBlocDelegate();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final WeatherApiClient weatherApiClient = WeatherApiClient(http.Client());
+  final WeatherRepository weatherRepository =
+      WeatherRepository(weatherApiClient);
+
+  runApp(App(
+    weatherRepository: weatherRepository,
+  ));
+}
+
+class App extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+
+  App({Key key, this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container());
   }
 }
-
-
---
+++ flutter_weather/lib/simple_bloc_delegate.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+
+class SimpleBlocDelegate extends BlocDelegate {
+
+  @override
+  void onTransition(Transition transition) {
+    // TODO: implement onTransition
+  }
+
+}
